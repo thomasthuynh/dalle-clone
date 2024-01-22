f5f52d100e8d1fe5dd7375ea00b14bc3d20869fb
ThomasHuynh <Thomas_TTH@live.com>
2024-01-22 13:18:31 -0500

    Styled and implemented data rendering logic for Home component

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,6 @@
 import Nav from "./Nav";
+import Card from "./Card";
+import FormField from "./FormField";
+import Loader from "./Loader";
 
-export { Nav };
+export { Nav, Card, FormField, Loader };
